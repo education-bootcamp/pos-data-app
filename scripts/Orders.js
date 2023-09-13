9a04e2caff1c0842b021e2da7d9fde35fae6f4a7
ozen-hub <info.ozenhub@gmail.com>
2023-09-13 23:58:11 +0530

    14

---
+++ scripts/Orders.js
@@ -0,0 +1,28 @@
+const loadData=()=>{
+    $('#orders-table-body').empty();
+
+    const firestore = firebase.firestore();
+    firestore
+        .collection('orders')
+        .get().then((result)=>{
+        result.forEach((records)=>{
+            const data = records.data();
+            const row=`
+                <tr>
+                    <td>${records.id}</td>
+                    <td>${data.customer.name}</td>
+                    <td>${data.orderDate}</td>
+                    <td>${data.totalCost}</td>
+                    <td>
+                        <button class="btn btn-dark btn-sm" onclick="printData('${records.id}')">Print</button>
+                    </td>
+                </tr>
+                `;
+            $('#orders-table-body').append(row);
+        });
+    });
+}
+
+const printData=()=>{
+
+}
