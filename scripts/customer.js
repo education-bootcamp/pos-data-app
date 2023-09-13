1f09b8b508b38f737d0b22cfb2dc414ed550c68e
ozen-hub <info.ozenhub@gmail.com>
2023-09-13 20:39:33 +0530

    1

---
+++ scripts/customer.js
@@ -0,0 +1,26 @@
+// function Customer(name,address,salary){
+//     this.name=name;
+//     this.address=address;
+//     this.salary=salary;
+// }
+
+createCustomer=()=>{
+    const tempCustomer = {
+        name: $('#name').val(),
+        address: $('#address').val(),
+        salary: $('#salary').val()
+    };
+
+    console.log(tempCustomer);
+
+    const database = firebase.firestore();
+    database
+        .collection('customers')
+        .add(tempCustomer)
+        .then((response)=>{
+            console.log(response);
+        }).catch((error)=>{
+        console.log(error);
+    });
+
+}
