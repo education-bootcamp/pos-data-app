93c20e0176bf7911dcd8cd6d0f0ae36d3d5389a0
ozen-hub <info.ozenhub@gmail.com>
2023-09-13 21:45:08 +0530

    5

--- scripts/customer.js
+++ scripts/customer.js
@@ -26,6 +26,9 @@ const createCustomer=()=>{
 }
 
 const loadCustomers=()=>{
+
+    $('#table-body').empty();
+
     const firestore = firebase.firestore();
     firestore
         .collection('customers')
@@ -39,21 +42,50 @@ const loadCustomers=()=>{
                     <td>${data.address}</td>
                     <td>${data.salary}</td>
                     <td>
-                        <button class="btn btn-danger btn-sm" onclick="deleteData(${records.id})">Delete</button> |
-                        <button class="btn btn-success btn-sm" onclick="updateData(${records.id})">Update</button>
+                        <button class="btn btn-danger btn-sm" onclick="deleteData('${records.id}')">Delete</button> |
+                        <button class="btn btn-success btn-sm" onclick="updateData('${records.id}')">Update</button>
                     </td>
                 </tr>
                 `;
                 $('#table-body').append(row);
-            })
-    })
+            });
+    });
 
 
 
 }
-
+customerId=undefined;
 const updateData=(id)=>{
+    customerId=id;
+    const firestore = firebase.firestore();
+    firestore
+        .collection('customers')
+        .doc(customerId)
+        .get().then((response)=>{
+            if (response.exists){
+                const data = response.data();
+                    $('#name').val(data.name);
+                    $('#address').val(data.address);
+                    $('#salary').val(data.salary)
+            }
 
+    })
+}
+const updateRecord=()=>{
+    if (customerId){
+        const firestore = firebase.firestore();
+        firestore
+            .collection('customers')
+            .doc(customerId)
+            .update({
+                name: $('#name').val(),
+                address: $('#address').val(),
+                salary: $('#salary').val()
+            }).then(()=>{
+            customerId=undefined;
+            loadCustomers();
+        })
+    }
 }
 
 const deleteData=(id)=>{
